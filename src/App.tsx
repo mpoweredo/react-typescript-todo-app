80c6a5aafafceb1e379464c714c50edff19a09a9
mpoweredo <mpow3r1337@gmail.com>
2022-07-28 14:49:43 +0200

    Added todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import TodoForm from "./components/TodoForm"
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App bg-slate-800 h-screen w-100 py-8 px-3 sm:px-10 flex items-start justify-center">
+      <TodoForm />
     </div>
   )
 }
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react'
+
+import { Todo } from '../types/types'
+
+const TodoForm = () => {
+    const todoInputRef = useRef<HTMLInputElement>(null)
+
+    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const todoTitle: string = todoInputRef.current?.value!
+
+        const todoObj: Todo = {
+            title: todoTitle,
+            done: false
+        }
+
+        todoInputRef!.current!.value = ''
+    }
+
+  return (
+    <div className='bg-slate-500 rounded w-full md:w-2/3 xl:w-2/4 p-4'>
+        <form className='flex w-100 gap-3' onSubmit={submitHandler}>
+            <input type="text" className='w-full bg-slate-600 rounded-sm text-white font-semibold text-lg px-3 outline-none border-transparent' ref={todoInputRef} />
+            <button className='px-4 py-2 bg-indigo-700 w-36 text-indigo-200 font-bold rounded-sm'>Add</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
